067640b6360c67f9145bcbc6119dc80008797aa4
George Brocklehurst <george@georgebrock.com>
2015-11-06 01:04:04 -0500

    Separate popup.js and background.js

--- background.js
+++ background.js
@@ -1,10 +1,30 @@
-chrome.browserAction.onClicked.addListener(function () {
+function copy(text) {
+  var target, range;
+
+  target = document.createElement("span");
+  target.innerHTML = text;
+  document.body.appendChild(target);
+  range = document.createRange();
+  range.selectNode(target);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand("copy");
+  document.body.removeChild(target);
+  window.getSelection().removeAllRanges();
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   var port = chrome.runtime.connectNative("org.passwordstore.pass");
 
   port.onMessage.addListener(function (msg) {
     console.log("Received: " + JSON.stringify(msg));
-    port.close();
+
+    sendResponse(msg);
+
+    copy(msg.password);
+    setTimeout(function () { console.log("clearing"); copy("-"); }, 5000);
   });
 
   port.postMessage({"domain": "zazzle.se"});
+  return true;
 });
--- popup.js
+++ popup.js
@@ -1,36 +1,18 @@
-var port = chrome.runtime.connectNative("org.passwordstore.pass");
+chrome.runtime.sendMessage(
+  {source: "popup", action: "load"},
+  function (response) {
+    console.log("HELLO!");
+    var extra, dl, dd;
 
-function copy(text) {
-  var target, range;
-
-  target = document.createElement("span");
-  target.innerHTML = text;
-  document.body.appendChild(target);
-  range = document.createRange();
-  range.selectNode(target);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(range);
-  document.execCommand("copy");
-  document.body.removeChild(target);
-  window.getSelection().removeAllRanges();
-}
-
-port.onMessage.addListener(function (msg) {
-  var extra, dl, dd;
-
-  document.getElementById("name").innerHTML = msg.name;
-  extra = document.getElementById("extra");
-  for (key in msg.extra) {
-    dl = document.createElement("dl");
-    dl.innerHTML = key;
-    dd = document.createElement("dd");
-    dd.innerHTML = msg.extra[key];
-    extra.appendChild(dl);
-    extra.appendChild(dd);
+    document.getElementById("name").innerHTML = response.name;
+    extra = document.getElementById("extra");
+    for (key in response.extra) {
+      dl = document.createElement("dl");
+      dl.innerHTML = key;
+      dd = document.createElement("dd");
+      dd.innerHTML = response.extra[key];
+      extra.appendChild(dl);
+      extra.appendChild(dd);
+    }
   }
-
-  copy(msg.password);
-  setTimeout(function () { copy("-"); }, 1000);
-});
-
-port.postMessage({"domain": "zazzle.se"});
+);
